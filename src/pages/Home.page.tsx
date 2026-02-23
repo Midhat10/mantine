746aa9d323c25e1f16c79b0a7182de9b88774eb0
Фархутдинов Мидхат Р. <Studentbgau1@gmail.com>
2026-02-23 23:02:37 +0300

    Начинаю работу с AppShell

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -2,6 +2,7 @@ import '@mantine/core';
 
 // eslint-disable-next-line no-duplicate-imports
 import { Flex } from '@mantine/core';
+import AppShellExample from '@/components/Appshell/AppShell';
 import Buttons from '@/components/Buttons/Buttons';
 import Cards from '@/components/Cards/Cards';
 import LightDarkButton from '@/components/LightDarkButton/LightDarkButton';
@@ -9,22 +10,12 @@ import LightDarkButton from '@/components/LightDarkButton/LightDarkButton';
 export function HomePage() {
   return (
     <>
+      <AppShellExample />
       <Flex direction="column" align="center" gap="lg">
         <Cards />
         <LightDarkButton />
         <Buttons />
       </Flex>
-
-      {/* <Group justify="center" mt="xl">
-        <Paper>
-          <Text>This is a simple text tag</Text>
-          <Text>This is a simpledsfsfd text tag</Text>
-        </Paper>
-        <Button>Hello World!</Button>
-        <Loader type="dots" />
-      </Group>
-      {/* <Welcome /> 
-      <ColorSchemeToggle /> */}
     </>
   );
 }
