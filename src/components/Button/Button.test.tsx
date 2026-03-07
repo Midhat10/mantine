26043ced11bcecf6ed52ea54104c50cca3b7d6b6
Фархутдинов Мидхат Р. <Studentbgau1@gmail.com>
2026-03-07 23:47:45 +0300

    ....

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,20 +1,16 @@
-import { render, screen } from '@testing-library/react';
-import { MantineProvider, Notifica } from '@mantine/core'; // Импортируем Notifications
-import { CounterProvider } from '../CounterContext/CounterContext'; // Импортируй свой контекст
+import { MantineProvider, PopoverProvider } from '@mantine/core';
+import { Notifications } from '@mantine/notifications';
 import ButtonExample from './ButtonExample';
 
-describe('ButtonExample component', () => {
-  it('renders the Cart button', () => {
-    render(
-      <MantineProvider>
-        <Notification />
-        <CounterProvider>
-          <ButtonExample />
-        </CounterProvider>
-      </MantineProvider>
-    );
+function App() {
+  return (
+    <MantineProvider>
+      <PopoverProvider>
+        <Notifications />
+        <ButtonExample />
+      </PopoverProvider>
+    </MantineProvider>
+  );
+}
 
-    const button = screen.getByRole('button', { name: /cart/i });
-    expect(button).toBeInTheDocument();
-  });
-});
+export default App;
