e83362fa4f0eed08f3a129d8a1682c03c4c2ce4d
Фархутдинов Мидхат Р. <Studentbgau1@gmail.com>
2026-03-08 02:23:02 +0300

    Теперь я запушил в гитхаб пейджес.

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,11 +1,8 @@
 import '@mantine/core';
 
 // eslint-disable-next-line no-duplicate-imports
-import { Flex } from '@mantine/core';
+
 import AppShellExample from '@/components/Appshell/AppShell';
-import Buttons from '@/components/Buttons/Buttons';
-import Cards from '@/components/Grid/Grid';
-import LightDarkButton from '@/components/LightDarkButton/LightDarkButton';
 
 export function HomePage() {
   return (
--- vite.config.mjs
+++ vite.config.mjs
@@ -5,6 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  base: '/mantine/',
   test: {
     globals: true,
     environment: 'jsdom',
