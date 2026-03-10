c604c5b0671a75c7b037a9688a25fae6c0d4be6a
Фархутдинов Мидхат Р. <Studentbgau1@gmail.com>
2026-03-10 23:14:36 +0300

    Попытка научиться делать хмм, ХОС

--- src/components/Appshell/AppShell.tsx
+++ src/components/Appshell/AppShell.tsx
@@ -1,50 +1,24 @@
 import { AppShell, Button, Flex, Text, Title } from '@mantine/core';
-import { useFetch } from '@mantine/hooks';
-import ButtonExample from '../Button/ButtonExample';
-import { CounterProvider } from '../CounterContext/CounterContext';
-import GridExample from '../Grid/Grid';
-import LightDarkButton from '../LightDarkButton/LightDarkButton';
-import PopOverExample from '../Popover/PopOver';
-
-interface Item {
-  id: string;
-  name: string;
-  price: number;
-  image: string;
-  category: string;
-}
 
 function AppShellExample() {
-  const { data } = useFetch<Item[]>(
-    'https://res.cloudinary.com/sivadass/raw/upload/v1535817394/json/products.json'
-  );
-  const initialCounters = data ? data.map(() => 1) : [];
   return (
     <>
-      <CounterProvider initialCounters={initialCounters}>
-        <AppShell padding={{ base: 10, sm: 15, lg: 'xl' }} header={{ height: 60 }}>
-          <AppShell.Header p="7.5px 20px">
-            <Flex align="center" gap="md" p="0">
-              <Text fw="600" fz="22px">
-                Vegetable
-              </Text>
-              <Button autoContrast variant="filled" bg="green" fw="500" fz="20px" radius="xl">
-                SHOP
-              </Button>
-              <LightDarkButton />
-              <PopOverExample>
-                <ButtonExample />
-              </PopOverExample>
-            </Flex>
-          </AppShell.Header>
+      <AppShell padding={{ base: 10, sm: 15, lg: 'xl' }} header={{ height: 60 }}>
+        <AppShell.Header p="7.5px 20px">
+          <Flex align="center" gap="md" p="0">
+            <Text fw="600" fz="22px">
+              Vegetable
+            </Text>
+            <Button autoContrast variant="filled" bg="green" fw="500" fz="20px" radius="xl">
+              SHOP
+            </Button>
+          </Flex>
+        </AppShell.Header>
 
-          <AppShell.Main bg="#f3f5fa">
-            <Title>Catalog</Title>
-
-            <GridExample data={data} length={0} />
-          </AppShell.Main>
-        </AppShell>
-      </CounterProvider>
+        <AppShell.Main bg="#f3f5fa">
+          <Title>Catalog</Title>
+        </AppShell.Main>
+      </AppShell>
     </>
   );
 }
--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,13 +1,65 @@
 import '@mantine/core';
 
-// eslint-disable-next-line no-duplicate-imports
-
+import { Children, cloneElement, ReactElement, useState } from 'react';
 import AppShellExample from '@/components/Appshell/AppShell';
+
+const Button = ({ style }: { style?: object }): ReactElement => (
+  <button
+    type="button"
+    style={{
+      color: 'red',
+      border: '1px solid red',
+      ...style,
+    }}
+  >
+    i am button
+  </button>
+);
+
+const Div = ({ style }: { style?: object }): ReactElement => <div style={style}>I am div</div>;
+
+const getRandomColor = () => {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+
+const BackgroundColorClick = ({ children }: { children: ReactElement | ReactElement[] }) => {
+  const [backgroundColor, setBackGroundColor] = useState<string>('');
+  const onClick = () => setBackGroundColor(getRandomColor());
+
+  return (
+    <>
+      <span onClick={onClick}>
+        {Children.map(children, (child: ReactElement) =>
+          cloneElement(child, {
+            ...child.props, //нужды для добавления пропсов, кроме style, так как мы должны изменить style
+            style: {
+              ...(child.props.style ?? {}), //добавляем исходные стили
+              backgroundColor, //то, что меняется при нажатии кнопки
+            },
+          })
+        )}
+      </span>
+    </>
+  );
+};
 
 export function HomePage() {
   return (
     <>
-      <AppShellExample />
+      {/* <AppShellExample /> */}
+      <BackgroundColorClick>
+        <Button />
+      </BackgroundColorClick>
+      <BackgroundColorClick>
+        <Div />
+      </BackgroundColorClick>
+      <Button />
+      <Div />
     </>
   );
 }
