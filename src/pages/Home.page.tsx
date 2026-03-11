d461d2ce9305a7ec3039ce417b951d4531d4b36e
Фархутдинов Мидхат Р. <Studentbgau1@gmail.com>
2026-03-11 05:38:29 +0300

    Делал дропдауны в стиле Сложенных частей.

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,65 +1,30 @@
 import '@mantine/core';
 
-import { Children, cloneElement, ReactElement, useState } from 'react';
-import AppShellExample from '@/components/Appshell/AppShell';
+// Укажите правильный относительный путь к файлу, который создали выше
+import { Button, Dropdown, DropdownMenu } from '../components/Dropdown/Dropdown';
 
-const Button = ({ style }: { style?: object }): ReactElement => (
-  <button
-    type="button"
-    style={{
-      color: 'red',
-      border: '1px solid red',
-      ...style,
-    }}
-  >
-    i am button
-  </button>
-);
-
-const Div = ({ style }: { style?: object }): ReactElement => <div style={style}>I am div</div>;
-
-const getRandomColor = () => {
-  const letters = '0123456789ABCDEF';
-  let color = '#';
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-};
-
-const BackgroundColorClick = ({ children }: { children: ReactElement | ReactElement[] }) => {
-  const [backgroundColor, setBackGroundColor] = useState<string>('');
-  const onClick = () => setBackGroundColor(getRandomColor());
-
+function RoleDropdown() {
   return (
-    <>
-      <span onClick={onClick}>
-        {Children.map(children, (child: ReactElement) =>
-          cloneElement(child, {
-            ...child.props, //нужды для добавления пропсов, кроме style, так как мы должны изменить style
-            style: {
-              ...(child.props.style ?? {}), //добавляем исходные стили
-              backgroundColor, //то, что меняется при нажатии кнопки
-            },
-          })
-        )}
-      </span>
-    </>
+    <Dropdown>
+      <DropdownMenu.Trigger>
+        <Button variant="soft">
+          Choose role
+          <DropdownMenu.TriggerIcon />
+        </Button>
+      </DropdownMenu.Trigger>
+      <Dropdown.List>
+        <Dropdown.Item>Admin</Dropdown.Item>
+        <Dropdown.Item>Client</Dropdown.Item>
+        <Dropdown.Item>Superadmin</Dropdown.Item>
+      </Dropdown.List>
+    </Dropdown>
   );
-};
+}
 
 export function HomePage() {
   return (
     <>
-      {/* <AppShellExample /> */}
-      <BackgroundColorClick>
-        <Button />
-      </BackgroundColorClick>
-      <BackgroundColorClick>
-        <Div />
-      </BackgroundColorClick>
-      <Button />
-      <Div />
+      <RoleDropdown />
     </>
   );
 }
