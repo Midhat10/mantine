a1a5a6cbd734517896deb7a4e006591eb826058e
Фархутдинов Мидхат Р. <Studentbgau1@gmail.com>
2026-03-23 18:48:51 +0300

    Поработал с useTransition

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,16 +1,44 @@
-import { useState } from 'react';
+import { useState, useTransition } from 'react';
 
 import './Home.page.css';
 
-import Header from '@/components/Header/Header';
-import Main from '@/components/Main/Main';
-
 export function HomePage() {
-  const [counter, setCounter] = useState(1);
+  const [searchResult, setSearchResult] = useState<Array<number>>([]);
+  const [searchCount, setSearchCount] = useState(0);
+  const [isPending, startTransition] = useTransition();
+  const [showSearchHistory, setShowSearchHistory] = useState(false);
+
+  const makeSearch = () => {
+    setSearchCount((prevCount) => prevCount + 1);
+    startTransition(() => {
+      setSearchResult(
+        Array(20000)
+          .fill(undefined)
+          .map((_, i) => i + 1)
+      );
+    });
+  };
+
+  console.log('counter');
+
   return (
-    <div className="main-container">
-      <Header setCounter={setCounter} counter={counter} />
-      <Main counter={counter} />
+    <div>
+      <button onClick={makeSearch}>
+        {searchCount === 0 ? 'Search' : `Search ${searchCount} times`}
+      </button>
+      <button onClick={() => setShowSearchHistory(true)}>Search History</button>
+      {showSearchHistory ? (
+        <section> Your search results</section>
+      ) : (
+        <section>
+          {isPending ? 'Loading...' : null}
+          <ul>
+            {searchResult.map((item) => (
+              <li key={item}>{item}</li>
+            ))}
+          </ul>
+        </section>
+      )}
     </div>
   );
 }
