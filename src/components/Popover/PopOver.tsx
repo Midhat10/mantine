2f6fcc26da947d95a4ef21b2f9bbcae871256314
Фархутдинов Мидхат Р. <Studentbgau1@gmail.com>
2026-05-10 20:22:09 +0300

    сделал оверлей для маленького меню

--- src/components/Popover/PopOver.tsx
+++ src/components/Popover/PopOver.tsx
@@ -7,9 +7,18 @@ interface PopOverProps {
 
 function PopOver({ children }: PopOverProps) {
   return (
-    <MPopover offset={24} trapFocus position="bottom" withArrow shadow="md">
+    <MPopover
+      offset={24}
+      trapFocus
+      position="bottom"
+      withArrow
+      shadow="md"
+      closeOnClickOutside
+      closeOnEscape
+      defaultOpened
+    >
       {children}
-      <MPopover.Dropdown p={0} bdrs={16}>
+      <MPopover.Dropdown p={0} bdrs={16} style={{ maxHeight: '300px', overflowY: 'overlay' }}>
         <GridSmall />
       </MPopover.Dropdown>
     </MPopover>
